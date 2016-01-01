740bf1fe4fcd3a984afa1247ff71150779c86107
Daniel Mejia <damsanto@gmail.com>
2016-01-01 22:24:00 +0000

    Created contact form view

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,11 @@
 ActiveRecord::Schema.define(version: 20160101201404) do
 
   create_table "contacts", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "comments"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
